761a56ad66c285f032d75dd8ac52fc21a5b5bf72
KevinBorgiani <kborgiani@gmail.com>
2025-06-22 06:50:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,20 @@ import pyautogui as bot
 import keyboard as kb
 import time
 
-time.sleep(5)
+time.sleep(3)
 quantas_kills = 10000
-armazem = "/armazem"
-sell_all = (713, 342)
+sell = (569, 273)
+
+# Você precisa de um client com keybind para usar esse macro.
+# Após ter instalado um, configure o a tecla 'delete'
+# para o seguinte comando '/armazem'
 
 for i in range(quantas_kills):
-    time.sleep(10)
+    time.sleep(7)
     bot.leftClick()
-    time.sleep(1)
-    bot.hotkey('enter')
-    time.sleep(1)
-    kb.write(armazem)
-    time.sleep(1)
-    bot.hotkey('enter')
-    time.sleep(1)
-    bot.moveTo(sell_all)
+    time.sleep(0.5)
+    bot.hotkey('delete')
+    time.sleep(0.5)
+    bot.moveTo(sell)
     time.sleep(1)
     bot.leftClick()
